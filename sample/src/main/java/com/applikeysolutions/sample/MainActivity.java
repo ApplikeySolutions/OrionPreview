2eb8d2e5aa260deb655b5476012a8af513b8bd8f
ostapenko-yuriy <ostapenko1990yura@gmail.com>
2017-07-04 11:07:44 +0300

    Sample UI changed

--- sample/src/main/java/com/applikeysolutions/sample/MainActivity.java
+++ sample/src/main/java/com/applikeysolutions/sample/MainActivity.java
@@ -3,10 +3,14 @@ package com.applikeysolutions.sample;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.util.DisplayMetrics;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -16,21 +20,18 @@ import com.applikeysolutions.animation.animation.TranslationAnimation;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.imgIcon)
-    ImageView imgIcon;
-    @BindView(R.id.activity_main)
-    RelativeLayout activityMain;
-    @BindView(R.id.tv)
-    TextView tv;
-    @BindView(R.id.imgBackground)
-    ImageView imgBackground;
-    private float screenHeight;
-    private float screenWidth;
-    private Bitmap blurredBitmap;
+    //Views
+    @BindView(R.id.cv_product_details) CardView cvProductDetails;
+    @BindView(R.id.ll_product_details) LinearLayout llProductDetails;
+    @BindView(R.id.rl_container) RelativeLayout rlContainer;
+    @BindView(R.id.tv_product_title) TextView tvProductTitle;
+    @BindView(R.id.imgBackground) ImageView imgBackground;
 
+    //Animations
     private ScaleAnimation increaseAnimationImage;
     private ScaleAnimation decreaseAnimationImage;
     private ScaleAnimation increaseAnimationText;
@@ -41,37 +42,67 @@ public class MainActivity extends AppCompatActivity {
     private TranslationAnimation arcDownAnimationTv;
     private BlurAnimation blurAnimation;
 
+    private float screenHeight;
+    private float screenWidth;
+    private Bitmap blurredBitmap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         ButterKnife.bind(this);
+
+        getScreenSize();
+        setProductBottomMargin();
+    }
+
+    @OnClick(R.id.cv_product_details)
+    void onProductDetailsClick() {
+        upAnimationImageView.showAnimation();
+        arcUpAnimationTv.showAnimation();
+        increaseAnimationImage.showAnimation();
+        increaseAnimationText.showAnimation();
+
+        cvProductDetails.setClickable(false);
+        rlContainer.setClickable(true);
+        addBlur();
+        tvProductTitle.setPivotX(tvProductTitle.getWidth());
+    }
+
+    @OnClick(R.id.rl_container)
+    void onContainerClick() {
+        downAnimationImageView.showAnimation();
+        arcDownAnimationTv.showAnimation();
+        decreaseAnimationImage.showAnimation();
+        decreaseAnimationText.showAnimation();
+
+        rlContainer.setClickable(false);
+        cvProductDetails.setClickable(true);
+        removeBlur();
+    }
+
+    private void getScreenSize(){
         DisplayMetrics displaymetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
         screenHeight = displaymetrics.heightPixels;
         screenWidth = displaymetrics.widthPixels;
+    }
 
-        imgIcon.setOnClickListener(v -> {
-            upAnimationImageView.showAnimation();
-            arcUpAnimationTv.showAnimation();
-            increaseAnimationImage.showAnimation();
-            increaseAnimationText.showAnimation();
+    private void setProductBottomMargin(){
+        llProductDetails.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+                    llProductDetails.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                } else {
+                    llProductDetails.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                }
 
-            imgIcon.setClickable(false);
-            activityMain.setClickable(true);
-            addBlur();
-            tv.setPivotX(tv.getWidth());
-        });
-
-        activityMain.setOnClickListener(v -> {
-            downAnimationImageView.showAnimation();
-            arcDownAnimationTv.showAnimation();
-            decreaseAnimationImage.showAnimation();
-            decreaseAnimationText.showAnimation();
-
-            activityMain.setClickable(false);
-            imgIcon.setClickable(true);
-            removeBlur();
+                RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) cvProductDetails.getLayoutParams();
+                params.bottomMargin = llProductDetails.getMeasuredHeight() * -1;
+                cvProductDetails.setLayoutParams(params);
+            }
         });
     }
 
@@ -81,44 +112,36 @@ public class MainActivity extends AppCompatActivity {
         initAnimation();
     }
 
-
     private void initAnimation() {
-        tv.post(() -> {
-            increaseAnimationImage = new ScaleAnimation.ScaleAnimationBuilder(imgBackground, 1.5f, 1.5f)
+        tvProductTitle.post(() -> {
+            increaseAnimationImage = new ScaleAnimation.ScaleAnimationBuilder(imgBackground, 1.5f, 1.5f).build();
+            increaseAnimationText = new ScaleAnimation.ScaleAnimationBuilder(tvProductTitle, 2f, 2f).build();
+            decreaseAnimationImage = new ScaleAnimation.ScaleAnimationBuilder(imgBackground, 1f, 1f).build();
+            decreaseAnimationText = new ScaleAnimation.ScaleAnimationBuilder(tvProductTitle, 1f, 1f).build();
+            upAnimationImageView = new TranslationAnimation.TranslationAnimationBuilder(cvProductDetails,
+                    TranslationAnimation.TranslationMode.TranslationY, 0, -(screenHeight / 2.2f))
                     .build();
-            increaseAnimationText = new ScaleAnimation.ScaleAnimationBuilder(tv, 1.5f, 1.5f)
-                    .build();
-            decreaseAnimationImage = new ScaleAnimation.ScaleAnimationBuilder(imgBackground, 1f, 1f)
-                    .build();
-            decreaseAnimationText = new ScaleAnimation.ScaleAnimationBuilder(tv, 1f, 1f)
-                    .build();
-            upAnimationImageView = new TranslationAnimation.TranslationAnimationBuilder(imgIcon,
-                    TranslationAnimation.TranslationMode.TranslationY, 0, -(screenHeight / 3))
-                    .build();
-            arcUpAnimationTv = new TranslationAnimation.TranslationAnimationBuilder(tv,
-                    TranslationAnimation.TranslationMode.TranslationAll, 0, screenHeight / 5)
+            arcUpAnimationTv = new TranslationAnimation.TranslationAnimationBuilder(tvProductTitle,
+                    TranslationAnimation.TranslationMode.TranslationAll, 0, screenHeight / 8)
                     .arcMode(TranslationAnimation.ArcMode.ArcUpward)
                     .additionStartPoint(0)
-                    .additionEndPoint(screenWidth / 2 - tv.getWidth() / 2)
+                    .additionEndPoint(screenWidth / 2 - tvProductTitle.getWidth() / 2)
                     .build();
-            downAnimationImageView = new TranslationAnimation.TranslationAnimationBuilder(imgIcon,
+            downAnimationImageView = new TranslationAnimation.TranslationAnimationBuilder(cvProductDetails,
                     TranslationAnimation.TranslationMode.TranslationY, -(screenHeight / 3), 0)
                     .build();
-            arcDownAnimationTv = new TranslationAnimation.TranslationAnimationBuilder(tv,
+            arcDownAnimationTv = new TranslationAnimation.TranslationAnimationBuilder(tvProductTitle,
                     TranslationAnimation.TranslationMode.TranslationAll, screenHeight / 5, 0)
                     .arcMode(TranslationAnimation.ArcMode.ArcDownard)
-                    .additionStartPoint(screenWidth / 2 - tv.getWidth() / 2)
+                    .additionStartPoint(screenWidth / 2 - tvProductTitle.getWidth() / 2)
                     .additionEndPoint(0)
                     .build();
-            blurAnimation = new BlurAnimation.BlurAnimationBuilder(0.4f, 7f)
-                    .build();
 
-            Bitmap originalBitmap = BitmapFactory.decodeResource(getResources(),
-                    R.drawable.sample_background);
+            blurAnimation = new BlurAnimation.BlurAnimationBuilder(0.4f, 7f).build();
+            Bitmap originalBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.sample_background);
             blurredBitmap = blurAnimation.blur(MainActivity.this, originalBitmap);
         });
     }
-
 
     private void addBlur() {
         imgBackground.setImageBitmap(new BitmapDrawable(getResources(), blurredBitmap).getBitmap());
