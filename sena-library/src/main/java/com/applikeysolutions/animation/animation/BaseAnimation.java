f43689660d560e0d18ab56d6bb699107d7c64f38
P.Shashkov <shashpash@gmail.com>
2017-09-28 12:41:24 +0300

    ui update

--- sena-library/src/main/java/com/applikeysolutions/animation/animation/BaseAnimation.java
+++ sena-library/src/main/java/com/applikeysolutions/animation/animation/BaseAnimation.java
@@ -2,9 +2,10 @@ package com.applikeysolutions.animation.animation;
 
 public abstract class BaseAnimation {
     public static final long LEAVE_ANIMATION_DURATION = 195L;
-    public static final long ENTER_ANIMATION_DURATION = 225L;
+    public static final long ENTER_ANIMATION_DURATION = 300L;
+    public static final long SMALL_ANIMATION_DOWNARD = 200;
     public static final long SMALL_ANIMATION_DURATION = 200;
-    public static final long LARGE_ANIMATION_DURATION = 375L;
+    public static final long LARGE_ANIMATION_DURATION = 350L;
 
     protected abstract void onStartAnimation();
 }
